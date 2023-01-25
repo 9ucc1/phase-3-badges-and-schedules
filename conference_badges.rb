3be640fa79ffbcf938ac5ab18d253e2b7be2e2eb
Gucci Fan <gucc@umich.edu>
2023-01-25 13:40:55 -0500

    commit tree 4f14eaf2ea2c8c894af75dfe216df9b1ceac286f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.each_with_index do |name, number|
+        "Hello, #{name}! You'll be assigned to room #{number+1}!"
+    end
+end
+
+#######help
+def printer(names)
+    batch_badge_creator(names).map do |name|
+        puts name
+    end
+
+    assign_rooms(names).map do |name|
+        puts name
+    end
+end
